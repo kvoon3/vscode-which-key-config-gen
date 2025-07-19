b6ea4eaa78419592473b352b2bcaf952fc8939b8
kvoon <kvoon@duck.com>
2025-07-19 18:35:31 +0800

    chore: switch to tsdown

--- src/configs.ts
+++ src/configs.ts
@@ -1,5 +1,5 @@
-import { defineConfigs } from 'reactive-vscode'
 import type { ScopedConfigKeyTypeMap } from './generated/meta'
+import { defineConfigs } from 'reactive-vscode'
 import { scopedConfigs } from './generated/meta'
 
 export const configs = defineConfigs<ScopedConfigKeyTypeMap>(
